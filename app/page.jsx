89c82ffbac496d2e94b0152d6543779e9fc5916b
Abhay Maurya <abhaymaurya821@gmail.com>
2026-01-10 17:54:59 +0530

    feat: Update Navbar component with logo and sticky positioning; enhance HomePage layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,6 @@
+import Footer from "@/components/common/Footer";
+import Loader from "@/components/common/Loader";
+import Navbar from "@/components/common/Navbar";
 import Hero from "@/sections/Hero";
 
 
@@ -6,7 +9,10 @@ import Hero from "@/sections/Hero";
 export default function HomePage() {
   return (
     <main>
+      {/* <Loader/> */}
+      <Navbar/>
       <Hero/>
+      {/* <Footer/> */}
     </main>
   );
 }
--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -1,16 +1,27 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { signOut, useSession } from "next-auth/react";
 
 export default function Navbar() {
   const { data: session } = useSession();
 
   return (
-    <nav className="bg-white border-b px-6 py-4 flex justify-between items-center">
-      <Link href="/" className="font-bold text-lg">
-        ServiceHub
-      </Link>
+    <nav className=" border-b px-6 py-4 flex justify-between items-center top-0 sticky bg-white/50">
+      <div className="flex justify-center items-center gap-3 ">
+        <Image
+          src="/favicon.svg"
+          alt="logo"
+          width={50}
+          height={50}
+          priority
+          className="rounded-full"
+        />
+        <Link href="/" className="font-bold text-lg">
+          Smart Service Marketplace
+        </Link>
+      </div>
 
       <div className="flex gap-4 items-center">
         <Link href="/services">Services</Link>
@@ -18,10 +29,7 @@ export default function Navbar() {
         {session ? (
           <>
             <Link href="/dashboard">Dashboard</Link>
-            <button
-              onClick={() => signOut()}
-              className="text-sm text-red-500"
-            >
+            <button onClick={() => signOut()} className="text-sm text-red-500">
               Logout
             </button>
           </>
